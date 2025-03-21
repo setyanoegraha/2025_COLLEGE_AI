9cfdec7e0ba8eddef7028c2cd2a44ecf07ab3f22
setyanoegraha <pandu4688@gmail.com>
2025-03-21 10:24:20 +0700

    Adding new directory jobsheet 2 and it's file

---
+++ jobsheet_02/latihan1.py
@@ -0,0 +1,39 @@
+# Cek Tipe Data
+var = 123
+print(type(var))
+
+# Tipe data String
+a = '123'
+b = '3.4'
+c = "Data String"
+print(type(c))
+
+# Tipe data numerik
+var1 = 345
+var2 = 34.5
+f1 = float(var1)
+print(type(f1))
+print(f1)
+
+sum = var1 + var2
+print(sum)
+
+var3 = 12.3
+var4 = 12.9
+i1 = int(var2)
+i2 = int(var3)
+print(i1)
+print(i2)
+
+# Tipe Data Boolean
+print(var1 == var2)
+print(var1 < var2)
+print(var1 > var2)
+print(var1 != var2)
+
+# Tipe Data List
+list = ["belajar dasar python", 2023, 3.8, True]
+print(list)
+print(list[3])
+print(list[1:3])
+print(len(list))
---
+++ jobsheet_02/latihan2.py
@@ -0,0 +1,27 @@
+import math
+
+# Mendapatkan nilai minimum dan maksimum
+x = min(5, 10, 25)
+y = max(6, 12, 18)
+print("(x,y)")
+print(f"({x},{y})")
+
+# Mendapatkan nilai absolut
+x = abs(-7.25)
+print(x)
+
+# Medapatkan nilai pangkat
+x = math.pow(4, 3)
+print(x)
+
+# Mendapatkan nilai akar
+x = math.sqrt(64)
+print(x)
+
+# Mendapatkan nilai pembulatan
+x = math.ceil(1.4)
+print(x)
+x = math.floor(1.4)
+print(x)
+
+# Mendapatkan nilai Konstanta (PI)
