5effb2683329882d581253daf938e81a186eb594
setyanoegraha <pandu4688@gmail.com>
2025-03-21 11:15:17 +0700

    Update tugas_praktikum2.py

---
+++ jobsheet_02/tugas_praktikum2.py
@@ -0,0 +1,11 @@
+import math
+
+# Menghitung Faktorial 6
+fact_6 = math.factorial(6)
+print(f"6!= {fact_6}\n")
+
+# Menghitung Faktorial 5
+fact_8 = math.factorial(8)
+fact_5 = math.factorial(5)
+hasil = fact_8 / fact_5
+print(f"8!/5!= {hasil}")
