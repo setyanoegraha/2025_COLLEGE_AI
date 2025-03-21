e2ffbf4e6f82e133b2efd1285109d1026e77a1e8
setyanoegraha <pandu4688@gmail.com>
2025-03-21 10:32:58 +0700

    Update latihhan2.py

--- jobsheet_02/latihan2.py
+++ jobsheet_02/latihan2.py
@@ -5,23 +5,31 @@ x = min(5, 10, 25)
 y = max(6, 12, 18)
 print("(x,y)")
 print(f"({x},{y})")
+print(" ")
 
 # Mendapatkan nilai absolut
 x = abs(-7.25)
 print(x)
+print(" ")
 
 # Medapatkan nilai pangkat
 x = math.pow(4, 3)
 print(x)
+print(" ")
 
 # Mendapatkan nilai akar
 x = math.sqrt(64)
 print(x)
+print(" ")
 
 # Mendapatkan nilai pembulatan
 x = math.ceil(1.4)
 print(x)
 x = math.floor(1.4)
 print(x)
+print(" ")
 
 # Mendapatkan nilai Konstanta (PI)
+x = math.pi
+print(x)
+print(" ")
