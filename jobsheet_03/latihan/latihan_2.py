2bba49203a0bbbfd528b677f720f1f1d1ed6dff2
setyanoegraha <pandu4688@gmail.com>
2025-04-10 23:47:45 +0700

    refactor: clean up for better readability

--- jobsheet_03/latihan/latihan_2.py
+++ jobsheet_03/latihan/latihan_2.py
@@ -3,34 +3,28 @@ import numpy as np
 
 # a. Mendapatkan Nilai Range
 data = [2, 3, 7, 8, 9, 12, 15, 20, 50, 42]
-
 minimum = np.min(data)
 maximum = np.max(data)
 x = maximum - minimum
-
 print(f"Nilai minimum dari data {data} adalah {minimum}")
 print(f"Nilai maksimum dari data {data} adalah {maximum}")
 print(f"Nilai range dari data {data} adalah {x}\n")
 
-
 # b. Mendapatkan Nilai Percentile
 data = [90, 80, 70, 65, 110, 80, 83, 67, 94]
-
 x = np.percentile(data, 90)
-
 print(f"Nilai percentile ke-90 dari data {data} adalah {x}\n")
-
 
 # c. Mendapatkan Nilai Quartile dan Interquartile Range
 data = [3, 4, 4, 5, 5, 6, 6, 7, 8, 8, 9, 10]
 
 Q1 = np.median(data[:int(len(data)/2)])
 Q3 = np.median(data[int(len(data)/2):])
-IQR = Q3 - Q1
 
+IQR = Q3 - Q1
 print(f"Quartile 1 dari data {data} adalah {Q1}")
 print(f"Quartile 3 dari data {data} adalah {Q3}")
-print(f"Interquartile range dari data {data} adalah {IQR}\n")
+print(f"Interquartile range dari data {data} adalah {IQR}")
 
 
 # d. Mendapatkan Nilai Variance dan Standart Deviation
@@ -39,6 +33,6 @@ mean = np.mean(data)
 var = np.var(data)
 std = np.std(data)
 
-print(f"Nilai rata-rata dari data {data} adalah {mean:.2f}")
-print(f"Nilai variance dari data {data} adalah {var:.2f}")
-print(f"Nilai standart deviation dari data {data} adalah {std:.2f}\n")
+print(f"Nilai rata-rata dari data {data} adalah {mean}")
+print(f"Nilai variance dari data {data} adalah {var}")
+print(f"Nilai standart deviation dari data {data} adalah {std}")
